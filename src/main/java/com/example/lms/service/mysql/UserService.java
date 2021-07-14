672e5ddbc152700deee3d50c49d3d997848b32c0
dbeqiraj <dejvisbeqiraj06@gmail.com>
2021-07-14 14:27:11 +0200

    learning intellij git interaction.

--- src/main/java/com/example/lms/service/mysql/UserService.java
+++ src/main/java/com/example/lms/service/mysql/UserService.java
@@ -23,15 +23,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Service
 public class UserService implements UserInterface {
 
-	private static final String USERNAME_NOT_FOUND = "Username not found!";
+    private static final String USERNAME_NOT_FOUND = "Username not found!";
 
-	private static final Logger logger = LogManager.getLogger(UserService.class.getSimpleName());
+    private static final Logger logger = LogManager.getLogger(UserService.class.getSimpleName());
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository   userRepository;
 
-	@Autowired
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Override
 	public User save(User user) {
@@ -41,67 +41,67 @@ public class UserService implements UserInterface {
 			user.setActive(false);
 		}
 
-		return userRepository.save(user);
-	}
+        return userRepository.save(user);
+    }
 
-	@Override
-	public void delete(int id) {
-		userRepository.deleteById(id);
-	}
+    @Override
+    public void delete(int id) {
+        userRepository.deleteById(id);
+    }
 
-	@Override
-	public User find(int id) {
+    @Override
+    public User find(int id) {
 
-		User user = userRepository.findById(id).get();
-		user.setAuthorities(getAuthorities(user.getRoles()));
+        User user = userRepository.findById(id).get();
+        user.setAuthorities(getAuthorities(user.getRoles()));
 
-		return user;
-	}
+        return user;
+    }
 
-	public UserDetails findByUsername(String username) {
-		User user = userRepository.findByUsername(username);
-		return new UserCustody(user);
-	}
+    public UserDetails findByUsername(String username) {
+        User user = userRepository.findByUsername(username);
+        return new UserCustody(user);
+    }
 
-	@SuppressWarnings("rawtypes")
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    @SuppressWarnings("rawtypes")
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-		User user = userRepository.findByUsername(username);
-		if (user == null)
-			throw new UsernameNotFoundException(USERNAME_NOT_FOUND);
+        User user = userRepository.findByUsername(username);
+        if (user == null)
+            throw new UsernameNotFoundException(USERNAME_NOT_FOUND);
 
-		// user.setAuthorities(getAuthorities((List<Role>)
-		// user.getClass().asSubclass(com.example.lms.entity.jpa.User.class).getField("roles").get(user)));
-		return new UserCustody(user);
-	}
+        // user.setAuthorities(getAuthorities((List<Role>)
+        // user.getClass().asSubclass(com.example.lms.entity.jpa.User.class).getField("roles").get(user)));
+        return new UserCustody(user);
+    }
 
-	private String[] getAuthorities(List<Role> roles) {
+    private String[] getAuthorities(List<Role> roles) {
 
-		List<String> authorities = new ArrayList<>();
+        List<String> authorities = new ArrayList<>();
 
-		roles.stream().forEach(role -> {
+        roles.stream().forEach(role -> {
 
-			for (String authority : com.example.lms.util.Role.valueOf(role.getName()).getAuthorities()) {
-				authorities.add(authority);
-			}
-		});
-		return StringUtils.toStringArray(authorities);
-	}
+            for (String authority : com.example.lms.util.Role.valueOf(role.getName()).getAuthorities()) {
+                authorities.add(authority);
+            }
+        });
+        return StringUtils.toStringArray(authorities);
+    }
 
-	@Override
-	public BCryptPasswordEncoder getbCryptPasswordEncoder() {
-		return bCryptPasswordEncoder;
-	}
+    @Override
+    public BCryptPasswordEncoder getbCryptPasswordEncoder() {
+        return bCryptPasswordEncoder;
+    }
 
-	private String encodePassword(String password) {
-		return bCryptPasswordEncoder.encode(password);
-	}
+    private String encodePassword(String password) {
+        return bCryptPasswordEncoder.encode(password);
+    }
 
-	@Override
-	public void log(String method, String interchange, Object object) throws JsonProcessingException {
+    @Override
+    public void log(String method, String interchange, Object object) throws JsonProcessingException {
 
-		String log = method.concat(", " + interchange).concat(": " + new ObjectMapper().writeValueAsString(object));
-		logger.info(log);
-	}
+        String log = method.concat(", " + interchange).concat(": " + new ObjectMapper().writeValueAsString(object));
+        logger.info(log);
+    }
 }
